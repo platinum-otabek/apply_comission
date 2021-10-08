4deef3d3a0d453fcbd75ec6f04b772a96898960d
platinum-otabek <mr.tursinboev@gmail.com>
2021-10-08 12:16:55 +0500

    test validation

--- utils/getCustomerImage.js
+++ utils/getCustomerImage.js
@@ -2,9 +2,7 @@ const {check, header} = require('express-validator')
 
 exports.checkBodyImage_Token = function () {
     return [
-        header('user_token')
-            .notEmpty() 
-            .withMessage('user_token is requiredq')
+        
     ]
 }
 
